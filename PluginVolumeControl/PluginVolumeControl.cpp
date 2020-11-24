4a2411747e92e2c263ecad59cd315d324f7c86b6
Cyril-Meyer <contact@cyrilmeyer.eu>
2020-11-24 13:26:15 +0100

    Create PluginVolumeControl, an example of plugin for GenericVST

---
+++ PluginVolumeControl/PluginVolumeControl.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "PluginVolumeControl.h"
+
+void __stdcall GenericVSTProcessBlock(double** inputs, double** outputs, int nFrames, int nChans, double* params, int nParams)
+{
+    if (nParams == 1)
+    {
+        const double gain = params[0];
+
+        for (int s = 0; s < nFrames; s++) {
+            for (int c = 0; c < nChans; c++) {
+                outputs[c][s] = inputs[c][s] * gain;
+            }
+        }
+    }
+    else
+    {
+        for (int s = 0; s < nFrames; s++) {
+            for (int c = 0; c < nChans; c++) {
+                outputs[c][s] = inputs[c][s];
+            }
+        }
+    }
+}
+
---
+++ PluginVolumeControl/PluginVolumeControl.h
@@ -0,0 +1,2 @@
+#pragma once
+__declspec(dllexport) void __stdcall GenericVSTProcessBlock(double** inputs, double** outputs, int nFrames, int nChans, double* params, int nParams);
